204135c8b7fbb177d58b3d93467bacd42ffbd4e3
tiagolpadua <tiagolpadua@gmail.com>
2017-07-26 19:50:22 -0300

    ajustes

---
+++ src/test/java/com/caelum/calculadora/AllTests.java
@@ -0,0 +1,13 @@
+package com.caelum.calculadora;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CalculadoraTest.class })
+public class AllTests {
+
+}
+
+
--- src/test/java/com/caelum/calculadora/CalculadoraTest.java
+++ src/test/java/com/caelum/calculadora/CalculadoraTest.java
@@ -1,14 +1,47 @@
 package com.caelum.calculadora;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CalculadoraTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@BeforeClass
+	public static void runOnceBeforeClass() {
+		System.out.println("@BeforeClass - runOnceBeforeClass");
 	}
+	
+	@AfterClass
+    public static void runOnceAfterClass() {
+        System.out.println("@AfterClass - runOnceAfterClass");
+    }
+	
+	@Before
+    public void runBeforeTestMethod() {
+        System.out.println("@Before - runBeforeTestMethod");
+    }
+	
+	@After
+    public void runAfterTestMethod() {
+        System.out.println("@After - runAfterTestMethod");
+    }
 
+	@Test
+	public void testSoma() {
+		Calculadora calculadora = new Calculadora();
+        assertEquals(3, calculadora.soma(2, 1));
+		System.out.println("@Test - testSoma");
+	}
+	
+	@Test
+	public void testIncremento() {
+		System.out.println("@Test - testIncremento");
+		Calculadora calculadora = new Calculadora();
+        assertEquals(3, calculadora.incrementa(2));
+	}
 }
+
